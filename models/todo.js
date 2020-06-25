437300f4db65d0744d30fa906c3b9f5b0c3ce040
wei <fhaowei@yahoo.com.tw>
2020-06-25 21:24:51 +0800

    feat: add db migration and define model reference

--- models/todo.js
+++ models/todo.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Todo.belongsTo(models.User, { foreignKey: { allowNull: false } })
     }
   };
   Todo.init({
