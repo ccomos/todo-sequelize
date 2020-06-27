3ab773b112441ef85c7c392d3722ce7bfc7d9653
wei <fhaowei@yahoo.com.tw>
2020-06-27 23:18:42 +0800

    add authenticator middleware

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-const session = require('express-session')
 // 載入設定檔，要寫在 express-session 以後
-const passport = require('passport')
 const usePassport = require('./config/passport')
 const routes = require('./routes')
 const app = express()
 const PORT = 3000
-// 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
-usePassport(app)
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
@@ -22,6 +19,9 @@ app.use(session({
 }))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+// 呼叫 Passport 函式並傳入 app，這條要寫在路由之前, session之後
+usePassport(app)
+
 app.use(routes)
 
 app.listen(PORT, () => {
--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,11 @@ const home = require('./modules/home')
 const todos = require('./modules/todos')
 const users = require('./modules/users')
 //const auth = require('./modules/auth')
-//const { authenticator } = require('../middleware/auth')  // 掛載 middleware
+const { authenticator } = require('../middleware/auth')  // 掛載 middleware
 
-router.use('/todos', todos)
+router.use('/todos', authenticator, todos)
 router.use('/users', users)
 //router.use('/auth', auth)  // 掛載模組
-router.use('/', home)
+router.use('/', authenticator, home)
 
 module.exports = router
