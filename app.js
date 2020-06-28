d06078e0dc554e4402caa254bde1e4792e633ee1
wei <fhaowei@yahoo.com.tw>
2020-06-28 11:38:12 +0800

    feat: add facebook login and dotenv

--- app.js
+++ app.js
@@ -4,17 +4,21 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const flash = require('connect-flash')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 // 載入設定檔，要寫在 express-session 以後
 const usePassport = require('./config/passport')
 const routes = require('./routes')
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT
+
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(session({
-  secret: 'ThisIsMySecret',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 }))
--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,12 @@ const router = express.Router()
 const home = require('./modules/home')
 const todos = require('./modules/todos')
 const users = require('./modules/users')
-//const auth = require('./modules/auth')
+const auth = require('./modules/auth') // 引用facebook第三方認證模組
 const { authenticator } = require('../middleware/auth')  // 掛載 middleware
 
 router.use('/todos', authenticator, todos)
 router.use('/users', users)
-//router.use('/auth', auth)  // 掛載模組
+router.use('/auth', auth)  // 掛載facebook第三方認證模組
 router.use('/', authenticator, home)
 
 module.exports = router
